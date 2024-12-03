c8ee1a249c0a7d2fdaffc86ef8c7d2fd64789d3e
Eduardo Castilho <80223884+eduardocastilho@users.noreply.github.com>
2024-12-03 06:54:53 -0300

    site.config.js misconfig.

--- site.config.js
+++ site.config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Infrastructure Analyst",
     bio: "Cybersecurity, DFIR, OSINT.",
-    email: "eduardo.castilho@forensec.blog.br,
+    email: "eduardo.castilho@forensec.blog.br",
     linkedin: "ecastilho",
     github: "eduardocastilho",
     instagram: "eduardocastilho",
